14491627adcd662057538e07d9931d335782628a
Ahmet Mentese <ahmet.mentese@omnicommediagroup.com>
2022-08-24 13:57:40 +0200

    added hallo

--- omg-practice-repcreation/Program.cs
+++ omg-practice-repcreation/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Ben");
+            Console.WriteLine("Hallo");
         }
     }
 }
